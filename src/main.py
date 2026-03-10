d399d917cb09c19150395ce93adb7bcad2cbeb72
tokinirr <tokinirr@student.42antananarivo.mg>
2026-03-10 13:31:57 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,16 +2,9 @@ import pandas as pd
 from filters import apply_filters
 from utils import load_config
 
-
 cfg = load_config("config/config.yaml")
-
-
 df = pd.read_excel(cfg["input_file"])
-
-
 results = apply_filters(df, cfg["filters"])
-
-
 with pd.ExcelWriter(cfg["output_file"]) as writer:
 for name, data in results.items():
 data.to_excel(writer, sheet_name=name, index=False)
